7979715ba32d73c577011cea4d98e7c990121bee
mansoureh <mansoureh.1998@gmail.com>
2021-01-21 06:05:05 +0330

    add jquery and add move counter function

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,20 +1,29 @@
-//timer function
-setInterval(timeUpdate , 1000)
-function timeUpdate(){
-    minute = document.querySelector("#clock-minute").innerText;
-    second = document.querySelector("#clock-second").innerText;
- 
-    if(parseInt(second)===59)
-    {
-        var newMin = (parseInt(minute) + 1).toString();
-        if(newMin<10)
-            newMin="0" + newMin;
-        document.querySelector("#clock-minute").innerHTML = newMin;
-        document.querySelector("#clock-second").innerHTML = "00";
+$(document).ready(function () {
+  //timer function
+  setInterval(timeUpdate, 1000);
+  function timeUpdate() {
+    minute = $("#clock-minute").text();
+    second = $("#clock-second").text();
+
+    if (parseInt(second) === 59) {
+      var newMin = (parseInt(minute) + 1).toString();
+      if (newMin < 10) newMin = "0" + newMin;
+      $("#clock-minute").text(newMin);
+      $("#clock-second").text("00");
     } else {
-        newSec = (parseInt(second) + 1).toString();
-        if(newSec<10)
-            newSec="0" + newSec;
-        document.querySelector("#clock-second").innerHTML = newSec;
+      newSec = (parseInt(second) + 1).toString();
+      if (newSec < 10) newSec = "0" + newSec;
+      $("#clock-second").text(newSec);
     }
-}
+  }
+
+  //move counter function
+  var move = 0;
+
+  $(".main button").click(function () {
+    if (this.innerHTML != "") {
+      move = parseInt($(".move span").text()) + 1;
+      $(".move span").text(move);
+    }
+  });
+});
