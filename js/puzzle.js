22f3db41e31e0eef97e65bfd1442bb870e2982d7
mansoureh <mansoureh.1998@gmail.com>
2021-01-21 07:21:10 +0330

    check winning

--- js/puzzle.js
+++ js/puzzle.js
@@ -3,15 +3,15 @@ $(document).ready(function () {
   var _number = [];
   for (i = 0; i < 15; i++) {
     _number[i] = Math.floor(Math.random() * 15 + 1);
-    function checkNumber() {
+    function checkRepeat() {
       for (j = 0; j < i; j++) {
         if (_number[i] == _number[j]) {
           _number[i] = Math.floor(Math.random() * 15 + 1);
-          checkNumber();
+          checkRepeat();
         }
       }
     }
-    checkNumber();
+    checkRepeat();
   }
   //set random number in the buttons
   for (i = 0; i < 15; i++) {
@@ -37,60 +37,90 @@ $(document).ready(function () {
   }
 
   //move counter function
-  foundNullLocation()
+  foundNullLocation();
   var move = 0;
   $(".main button").click(function () {
-    if (this.innerHTML != "") {      
-      foundNullLocation()
+    if (this.innerHTML != "") {
+      foundNullLocation();
+
+      //change number
       switch ($(this).index()) {
-        case _top: {
-          $(".main button").eq(nullLoc).text($(".main button").eq(_top).text());
-          $(".main button").eq(_top).text("");
-          move = parseInt($(".move span").text()) + 1;
-        }
-        break
-        case _bottom: {
-          $(".main button").eq(nullLoc).text($(".main button").eq(_bottom).text());
-          $(".main button").eq(_bottom).text("");
-          move = parseInt($(".move span").text()) + 1;
-        }
-        break
-        case _left: {
-          $(".main button").eq(nullLoc).text($(".main button").eq(_left).text());
-          $(".main button").eq(_left).text("");
-          move = parseInt($(".move span").text()) + 1;
-        }
-        break
-        case _right: {
-          $(".main button").eq(nullLoc).text($(".main button").eq(_right).text());
-          $(".main button").eq(_right).text("");
-          move = parseInt($(".move span").text()) + 1;
-        }
-        break
+        case _top:
+          {
+            _number[_top] = "";
+            _number[nullLoc] = $(".main button").eq(_top).text();
+            $(".main button").eq(nullLoc).text(_number[nullLoc]);
+            $(".main button").eq(_top).text("");
+            move = parseInt($(".move span").text()) + 1;
+          }
+          break;
+        case _bottom:
+          {
+            _number[_bottom] = "";
+            _number[nullLoc] = $(".main button").eq(_bottom).text();
+            $(".main button").eq(nullLoc).text(_number[nullLoc]);
+            $(".main button").eq(_bottom).text("");
+            move = parseInt($(".move span").text()) + 1;
+          }
+          break;
+        case _left:
+          {
+            _number[_left] = "";
+            _number[nullLoc] = $(".main button").eq(_left).text();
+            $(".main button").eq(nullLoc).text(_number[nullLoc]);
+            $(".main button").eq(_left).text("");
+            move = parseInt($(".move span").text()) + 1;
+          }
+          break;
+        case _right:
+          {
+            _number[_right] = "";
+            _number[nullLoc] = $(".main button").eq(_right).text();
+            $(".main button").eq(nullLoc).text(_number[nullLoc]);
+            $(".main button").eq(_right).text("");
+            move = parseInt($(".move span").text()) + 1;
+          }
+          break;
       }
+
+      //change move counter
       $(".move span").text(move);
     }
+    checkWinning();
   });
 
   //buttons around of null
   (_left = null), (_top = null), (_bottom = null), (_right = null);
-  function foundNullLocation(){
+  function foundNullLocation() {
     for (nullLocation = 0; nullLocation <= 15; nullLocation++) {
-        if ($(".main button").eq(nullLocation).text() == "") {
-          nullLoc = nullLocation;
-          if (nullLocation % 4 != 0) {
-            _left = nullLocation - 1;
-          }
-          if (nullLocation > 3) {
-            _top = nullLocation - 4;
-          }
-          if (nullLocation < 12) {
-            _bottom = nullLocation + 4;
-          }
-          if (nullLocation % 4 != 3) {
-            _right = nullLocation + 1;
-          }
+      if ($(".main button").eq(nullLocation).text() == "") {
+        nullLoc = nullLocation;
+        if (nullLocation % 4 != 0) {
+          _left = nullLocation - 1;
+        }
+        if (nullLocation > 3) {
+          _top = nullLocation - 4;
+        }
+        if (nullLocation < 12) {
+          _bottom = nullLocation + 4;
+        }
+        if (nullLocation % 4 != 3) {
+          _right = nullLocation + 1;
         }
       }
+    }
+  }
+
+  //check winning
+  function checkWinning() {
+    let lose = 0;
+    for (i = 0; i < 15; i++) {
+      if (_number[i] != i + 1) {
+        lose++;
+      }
+    }
+    if (!lose) {
+      alert("you are winner !");
+    }
   }
 });
