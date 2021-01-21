2e6cd3402f78954fb3b491e1318257c456da0d4d
mansoureh <mansoureh.1998@gmail.com>
2021-01-21 07:06:30 +0330

    change numbers

--- js/puzzle.js
+++ js/puzzle.js
@@ -14,8 +14,8 @@ $(document).ready(function () {
     checkNumber();
   }
   //set random number in the buttons
-  for(i=0 ; i<15 ; i++){
-      $('.main button').eq(i).text(_number[i])
+  for (i = 0; i < 15; i++) {
+    $(".main button").eq(i).text(_number[i]);
   }
 
   //timer function
@@ -37,11 +37,60 @@ $(document).ready(function () {
   }
 
   //move counter function
+  foundNullLocation()
   var move = 0;
   $(".main button").click(function () {
-    if (this.innerHTML != "") {
-      move = parseInt($(".move span").text()) + 1;
+    if (this.innerHTML != "") {      
+      foundNullLocation()
+      switch ($(this).index()) {
+        case _top: {
+          $(".main button").eq(nullLoc).text($(".main button").eq(_top).text());
+          $(".main button").eq(_top).text("");
+          move = parseInt($(".move span").text()) + 1;
+        }
+        break
+        case _bottom: {
+          $(".main button").eq(nullLoc).text($(".main button").eq(_bottom).text());
+          $(".main button").eq(_bottom).text("");
+          move = parseInt($(".move span").text()) + 1;
+        }
+        break
+        case _left: {
+          $(".main button").eq(nullLoc).text($(".main button").eq(_left).text());
+          $(".main button").eq(_left).text("");
+          move = parseInt($(".move span").text()) + 1;
+        }
+        break
+        case _right: {
+          $(".main button").eq(nullLoc).text($(".main button").eq(_right).text());
+          $(".main button").eq(_right).text("");
+          move = parseInt($(".move span").text()) + 1;
+        }
+        break
+      }
       $(".move span").text(move);
     }
   });
+
+  //buttons around of null
+  (_left = null), (_top = null), (_bottom = null), (_right = null);
+  function foundNullLocation(){
+    for (nullLocation = 0; nullLocation <= 15; nullLocation++) {
+        if ($(".main button").eq(nullLocation).text() == "") {
+          nullLoc = nullLocation;
+          if (nullLocation % 4 != 0) {
+            _left = nullLocation - 1;
+          }
+          if (nullLocation > 3) {
+            _top = nullLocation - 4;
+          }
+          if (nullLocation < 12) {
+            _bottom = nullLocation + 4;
+          }
+          if (nullLocation % 4 != 3) {
+            _right = nullLocation + 1;
+          }
+        }
+      }
+  }
 });
